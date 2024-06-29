868a0ba41c702cbe41a01f68b71ed65a289b1c16
claireeey <tjxyn1013@gmail.com>
2024-06-29 12:23:21 +0100

    Basic_HTML_and_JavaScript_1.

---
+++ Basic_HTML_and_JavaScript_1./Basic_JavaScript_1.js
@@ -0,0 +1,5 @@
+function My_First_Function(){
+    var String = "Kiss me, I'm Irish!";
+    document.getElementById("Irish").innerHTML = String;
+    document.getElementById("Irish").style.color = "green";
+}
